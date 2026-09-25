0901348d9ae172e9e0b52c6813b9275a53beab84
hayikisemuel <hayikisemuel@users.noreply.github.com>
2026-09-25 12:06:12 -0600

    Make Poukitty installable on iPhone

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'poukitty-v1'
+
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()))
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone()
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+      return response
+    })),
+  )
+})
